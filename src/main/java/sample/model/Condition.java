f8e7cbc14d71d9fc615ea6b2f1251b5f8dbc89cf
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-17 17:11:48 +0100

    working maven

---
+++ src/main/java/sample/model/Condition.java
@@ -0,0 +1,7 @@
+package sample.model;
+
+/**
+ * Created by kmlsz on 17.11.2016.
+ */
+public class Condition {
+}
---
+++ src/main/java/sample/model/Weather.java
@@ -0,0 +1,29 @@
+package sample.model;
+
+
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by kmlsz on 17.11.2016.
+ */
+public class Weather {
+
+    private double temperature;
+    private LocalDateTime lastUpdated;
+    private double feelsLike;
+    private String iconURL;
+    private boolean isDay;
+    private double preasure;
+
+
+
+    /*
+    CONDITION = CLASS
+    condition:text
+    condition:icon
+    condition:code
+    pressure_mb
+    is_day
+     */
+}
