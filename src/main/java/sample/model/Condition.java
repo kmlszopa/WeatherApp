aa3a930ac9fd08a9b27b544026042ce7e242766c
Kamil Szopaan <kmlszopa@gmail.com>
2016-11-17 17:20:38 +0100

    added weather model

--- src/main/java/sample/model/Condition.java
+++ src/main/java/sample/model/Condition.java
@@ -4,4 +4,52 @@ package sample.model;
  * Created by kmlsz on 17.11.2016.
  */
 public class Condition {
+    private String text;
+
+    private String iconUrl;
+
+    public Condition(String text, String iconUrl, int code) {
+        this.text = text;
+        this.iconUrl = iconUrl;
+        this.code = code;
+    }
+
+    public Condition(String text, String iconUrl) {
+
+        this.text = text;
+        this.iconUrl = iconUrl;
+    }
+
+    public Condition(String text) {
+
+        this.text = text;
+    }
+
+    public String getText() {
+
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    private int code;
+
 }
--- src/main/java/sample/model/Weather.java
+++ src/main/java/sample/model/Weather.java
@@ -15,15 +15,82 @@ public class Weather {
     private String iconURL;
     private boolean isDay;
     private double preasure;
+    private String location;
+    private Condition condition;
 
+    public Weather(double temperature, LocalDateTime lastUpdated, double feelsLike, String iconURL, boolean isDay, double preasure, String location) {
+        this.temperature = temperature;
+        this.lastUpdated = lastUpdated;
+        this.feelsLike = feelsLike;
+        this.iconURL = iconURL;
+        this.isDay = isDay;
+        this.preasure = preasure;
+        this.location = location;
+        this.condition = condition;
+    }
 
+    public Condition getCondition() {
+        return condition;
+    }
 
-    /*
-    CONDITION = CLASS
-    condition:text
-    condition:icon
-    condition:code
-    pressure_mb
-    is_day
-     */
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    public double getTemperature() {
+
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public String getIconURL() {
+        return iconURL;
+    }
+
+    public void setIconURL(String iconURL) {
+        this.iconURL = iconURL;
+    }
+
+    public boolean isDay() {
+        return isDay;
+    }
+
+    public void setDay(boolean day) {
+        isDay = day;
+    }
+
+    public double getPreasure() {
+        return preasure;
+    }
+
+    public void setPreasure(double preasure) {
+        this.preasure = preasure;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
